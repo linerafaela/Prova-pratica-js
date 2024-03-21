6d6a9f76eba7c1f30136c19ea190d2ed1cb58e19
linerafaela <128156711+linerafaela@users.noreply.github.com>
2024-03-21 08:47:46 -0300

    Add files via upload

--- prova2.js
+++ prova2.js
@@ -2,9 +2,14 @@
 let clientes=['Juraci','Seu João','Tatinha','Lucas Reis']
 
 //Adicionando clientes
-frutas.push('Thauane')
-frutas.push('Didi Beli')
-frutas.push('Léo')
+clientes.push('Thauane')
+clientes.push('Didi Beli')
+clientes.push('Léo')
 
 //Tirando clientes mal amados
-frutas.pop(4)
+clientes.pop(4)
+
+//Sorteando o ganhador que vai levar sua compra grátis
+let indiceAleatorio = Math.floor(Math.random() *6);
+
+console.log('O ganhador do sorteio é:', clientes [indiceAleatorio])
